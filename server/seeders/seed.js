76873ffd9e6a1dd75c8838ba79fd9a4d464638a8
Aveheart <110785267+Aveheart@users.noreply.github.com>
2022-10-25 15:29:41 -0400

    dogwalkers to dogWalker

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -6,7 +6,7 @@ const DogWalkerData = require('./DogWalkerSeeds.json');
 db.once('open', async () => {
   await DogWalker.deleteMany({});
 
-  const dogwalkers = await DogWalker.insertMany(DogWalkerData);
+  const dogWalkers = await DogWalker.insertMany(DogWalkerData);
 
   console.log('Dog walkers seeded!');
   process.exit(0);
