a84d5fca1a47454d67ca29f734a32257ada811fb
Averi <averi.sherman@gmail.com>
2022-10-25 14:13:08 -0400

    seed.js

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,1 +1,13 @@
-seeds
+const db = require('../config/connection');
+const { DogWalker } = require('../models');
+
+const DogWalkerData = require('./DogWalkerSeeds.json');
+
+db.once('open', async () => {
+  await DogWalker.deleteMany({});
+
+  const dogwalkers = await DogWalker.insertMany(DogWalkerData);
+
+  console.log('Dog walkers seeded!');
+  process.exit(0);
+});
