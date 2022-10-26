11d8205cfe572b47c2a83c777856b206f56af88e
maiachargois <=>
2022-10-26 10:57:32 -0700

    client queries/form update

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,45 @@
 // ADDING SO FILES/FOLDERS WILL GET SENT TO GITHUB
 import { gql } from '@apollo/client';
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(userfullname: $userfullname) {
+      _id
+      userfullname
+      email
+      pets {
+        _id
+        petText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_PETS = gql`
+  query getPets {
+    Pets {
+      _id
+      petText
+      petUser
+      createdAt
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      Pets {
+        _id
+        petText
+        petUser
+        createdAt
+      }
+    }
+  }
+`;
