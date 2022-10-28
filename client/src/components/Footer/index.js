47737c43536c3912d7d5c8d1047e65f9be737383
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-10-27 17:38:36 -0700

    starting footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,1 +1,27 @@
-// ADDING SO FILES/FOLDERS WILL GET SENT TO GITHUB
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Footer = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    return (
+      <footer className="w-100 mt-auto bg-secondary p-4">
+        <div className="container text-center mb-5">
+          {location.pathname !== '/' && (
+            <button
+              className="btn btn-dark mb-3"
+              onClick={() => navigate(-1)}
+            >
+              &larr; Go Back
+            </button>
+          )}
+          <h4>
+            Made with 🤍 by your favorite dog walkers.
+          </h4>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
