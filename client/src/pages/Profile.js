c995390125294fdaaec5f34fc4b1198d43f8a306
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2022-10-30 18:05:54 -0700

    fixed casing and spelling continuity issues throughout

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Navigate, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 
-import Petform from "../components/PetForm";
-import Petlist from "../components/PetList";
+import PetForm from "../components/PetForm";
+import PetList from "../components/PetList";
 
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 
 import Auth from "../utils/auth";
 
 const Profile = () => {
-  const { username: userParam } = useParams();
+  const { userFullName: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { userFullName: userParam },
@@ -39,11 +39,11 @@ const Profile = () => {
     <div>
       <div className="flex-row justify-center mb-3">
         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.useFullName}'s` : "your"} profile.
+          Viewing {userParam ? `${user.userFullName}'s` : "your"} profile.
         </h2>
 
         <div className="col-12 col-md-10 mb-5">
-          <Petlist
+          <PetList
             pets={user.pets}
             title={`${user.userFullName}'s pets...`}
             showPetName={true}
@@ -55,7 +55,7 @@ const Profile = () => {
             className="col-12 col-md-10 mb-3 p-3"
             style={{ border: "1px dotted #1a1a1a" }}
           >
-            <Petform />
+            <PetForm />
           </div>
         )}
       </div>
