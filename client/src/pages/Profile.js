f9736d2af99b118e11ea13d44a9d743723668838
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2022-10-30 17:38:05 -0700

    changed camelcase and adjusted profile.js to match petlist.index variables

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,65 +2,65 @@ import React from "react";
 import { Navigate, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 
-import Petform from '../components/PetForm';
-import Petlist from '../components/PetList';
+import Petform from "../components/PetForm";
+import Petlist from "../components/PetList";
 
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 
 import Auth from "../utils/auth";
 
 const Profile = () => {
-    const { username: userParam } = useParams();
-  
-    const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-      variables: { userFullName: userParam },
-    });
-  
-    const user = data?.me || data?.user || {};
-    // navigate to personal profile page if username is yours
-    if (Auth.loggedIn() && Auth.getProfile().data.userFullName === userParam) {
-      return <Navigate to="/me" />;
-    }
-  
-    if (loading) {
-      return <div>Loading...</div>;
-    }
-  
-    if (!user?.userFullName) {
-      return (
-        <h4>
-          You need to be logged in to see this. Use the navigation links above to
-          sign up or log in!
-        </h4>
-      );
-    }
-  
+  const { username: userParam } = useParams();
+
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { userFullName: userParam },
+  });
+
+  const user = data?.me || data?.user || {};
+  // navigate to personal profile page if username is yours
+  if (Auth.loggedIn() && Auth.getProfile().data.userFullName === userParam) {
+    return <Navigate to="/me" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user?.userFullName) {
     return (
-      <div>
-        <div className="flex-row justify-center mb-3">
-          <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-            Viewing {userParam ? `${user.useFullName}'s` : 'your'} profile.
-          </h2>
-  
-          <div className="col-12 col-md-10 mb-5">
-            <Petlist
-              pets={user.pets}
-              title={`${user.userFullName}'s pets...`}
-              showTitle={false}
-              showuserFullName={false}
-            />
-          </div>
-          {!userParam && (
-            <div
-              className="col-12 col-md-10 mb-3 p-3"
-              style={{ border: '1px dotted #1a1a1a' }}
-            >
-              <Petform />
-            </div>
-          )}
-        </div>
-      </div>
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </h4>
     );
-  };
+  }
+
+  return (
+    <div>
+      <div className="flex-row justify-center mb-3">
+        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+          Viewing {userParam ? `${user.useFullName}'s` : "your"} profile.
+        </h2>
+
+        <div className="col-12 col-md-10 mb-5">
+          <Petlist
+            pets={user.pets}
+            title={`${user.userFullName}'s pets...`}
+            showPetName={true}
+            showUserFullName={true}
+          />
+        </div>
+        {!userParam && (
+          <div
+            className="col-12 col-md-10 mb-3 p-3"
+            style={{ border: "1px dotted #1a1a1a" }}
+          >
+            <Petform />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
