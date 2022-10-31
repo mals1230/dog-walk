34718329af552d261ec85807b92c90ff52657f2b
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2022-10-31 14:47:29 -0700

    re-aligned pet registration and bookWalk

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -7,25 +7,25 @@ import PetForm from "../components/PetForm";
 import WalkForm from "../components/WalkForm";
 
 const Pets = ({ pets }) => {
-
   return (
     <main>
       <div className="flex-row justify-center">
         <div
-          className="col-12 col-md-10 mb-3 p-3"
+          className="col-6 col-md-5 mb-3 p-3"
           style={{ border: "1px dotted #1a1a1a" }}
         >
           <PetForm />
+        </div>
+        <div
+          className="col-6 col-md-5 mb-3 p-3"
+          style={{ border: "1px dotted #1a1a1a" }}
+        >
           <WalkForm />
         </div>
         <div className="col-12 col-md-8 mb-3">
-          <p>My Pets</p>
-
-
+          <h5>My Registered Pets</h5>
           <PetList pets={pets} title="Pets registered" />
-
         </div>
-
       </div>
     </main>
   );
